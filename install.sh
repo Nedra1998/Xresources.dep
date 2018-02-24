55a440a0394986c4a6a5dde046025d34993617e6
Arden Rasmussen <ardenisthebest@gmail.com>
2018-02-23 17:58:13 -0800

    Modified install

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ printf "Light or Dark variant [dark]: "
 read var
 
 if [[ $var == "" ]]; then
-  var="dark"
+    var="dark"
 fi
 
 if [[ -e "$HOME/.Xresources" ]]; then
-  if [[ $var == "dark" ]]; then
-    cat ./Xresources_dark >> ~/.Xresources
-  else
-    cat ./Xresources_light >> ~/.Xresources
-  fi
+    if [[ $var == "dark" ]]; then
+        cat ./Xresources_dark >> ~/.Xresources
+    else
+        cat ./Xresources_light >> ~/.Xresources
+    fi
 else
-  if [[ $var == "dark" ]]; then
-    eval "cp ./Xresources_dark ~/.Xresources"
-  else
-    eval "cp ./Xresources_light ~/.Xresources"
-  fi
+    if [[ $var == "dark" ]]; then
+        eval "cp ./Xresources_dark ~/.Xresources"
+    else
+        eval "cp ./Xresources_light ~/.Xresources"
+    fi
 fi
